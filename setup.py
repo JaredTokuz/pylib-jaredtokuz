5d942409fbf7f2294ea6c91945b32536d493f884
JaredTokuz <jtokuz@gmail.com>
2021-11-13 15:18:11 -0600

    changed lib name

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="widely-used-functions",
+    name="pylibjaredtokuz",
     version="0.0.3",
     author="Jared Tokuz",
     author_email="jtokuz@gmail.com",
     description="Installation of Package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/JaredTokuz/WidelyUsedFunctions",
+    url="https://github.com/JaredTokuz/pylib-jaredtokuz",
     project_urls={
-        "Bug Tracker": "https://github.com/JaredTokuz/WidelyUsedFunctions/issues"
+        "Bug Tracker": "https://github.com/JaredTokuz/pylib-jaredtokuz/issues"
     },
     license="MIT",
-    packages=["widely-used-functions"],
+    packages=["pylibjaredtokuz"],
     install_requires=[],
 )
