338c98a4978b980d402973b56ba7497ebed6bafe
JaredTokuz <jtokuz@gmail.com>
2021-11-13 18:07:52 -0600

    updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pylibjaredtokuz",
-    version="0.0.3",
+    version="0.0.4",
     author="Jared Tokuz",
     author_email="jtokuz@gmail.com",
     description="Installation of Package",
